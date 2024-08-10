610973fda18b958b54514114ca208b51e48a6d02
unknown <h.ayed.amal@gmail.com>
2024-08-10 14:42:13 +0100

    first java program

---
+++ Java Fundamentals/practice/first java program/firstprogram.java
@@ -0,0 +1,8 @@
+public class Firstprogram{
+    public static void main(string[] args){
+       String name="Amal";
+       int age= I am 30 years old;
+       String town= My hometown is Bekalta-Monastir;
+       System.out.println( name age town);
+    }
+}
