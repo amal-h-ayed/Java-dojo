394e368a66bee5b294d6ddd29ae975eea2fada9a
unknown <h.ayed.amal@gmail.com>
2024-08-24 23:54:04 +0100

    SaveTravels

---
+++ Java Spring/core/SaveTravels/src/main/java/com/dojo/SaveTravels/SaveTravelsApplication.java
@@ -0,0 +1,14 @@
+package com.dojo.SaveTravels;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveTravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveTravelsApplication.class, args);
+	}
+
+}
---
+++ Java Spring/core/SaveTravels/src/test/java/com/dojo/SaveTravels/SaveTravelsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dojo.SaveTravels;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveTravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
