35295d9374d812748b7cd25de7aa4e517361214a
unknown <h.ayed.amal@gmail.com>
2024-08-31 01:15:15 +0100

    Login & Registration

---
+++ Java Spring/core/LoginRegistartion/src/main/java/com/codingdojo/LoginRegistration/LoginRegistartionApplication.java
@@ -0,0 +1,14 @@
+package com.codingdojo.LoginRegistration;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistartionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistartionApplication.class, args);
+	}
+
+}
---
+++ Java Spring/core/LoginRegistartion/src/test/java/com/codingdojo/LoginRegistration/LoginRegistartionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.codingdojo.LoginRegistration;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistartionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
